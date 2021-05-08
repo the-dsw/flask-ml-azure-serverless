cfb616cabcaabe349fc93b445338ed2c00366a34
mauricio oporto <fmod@the-dsw.com>
2021-05-08 18:18:15 +0200

    [UPDATE] make_predict name

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<flask-ml-seviceless>.azurewebsites.net:$PORT/predict
+     -X POST https://flask-ml-seviceless.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
